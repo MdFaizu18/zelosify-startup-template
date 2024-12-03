a48f48f206dfa4b604b3341f22daa3a71cd72494
Md Faizu <mohammed17092002@gmail.com>
2024-12-03 20:38:09 +0530

    dashboard

--- src/components/LandingPage/AnimatedDashboardPreview.jsx
+++ src/components/LandingPage/AnimatedDashboardPreview.jsx
@@ -99,10 +99,11 @@ const MacbookSearchBar = () => {
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                     strokeWidth="2"
-                                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                    d="M9 5l7 7-7 7"
                                 ></path>
                             </svg>
                         </button>
+
                     </div>
                 </div>
 
